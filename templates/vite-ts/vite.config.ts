d0fff12827aa1956febc6e94f32acf4d16f83c5a
qmhc <40221744+qmhc@users.noreply.github.com>
2023-10-31 09:05:21 +0800

    feat(vite-ts): update config template of Vite

--- templates/vite-ts/vite.config.ts
+++ templates/vite-ts/vite.config.ts
@@ -5,26 +5,36 @@ import autoImport from 'unplugin-auto-import/vite'
 import components from 'unplugin-vue-components/vite'
 import { VexipUIResolver } from '@vexip-ui/plugins'
 
-export default defineConfig({
-  optimizeDeps: {
-    include: ['vexip-ui', 'vexip-ui/es/css/**/*.js', 'vexip-ui/es/style/**/*.js', '@vexip-ui/icons']
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    autoImport({
-      vueTemplate: true,
-      resolvers: [VexipUIResolver()],
-      imports: [
-        {
-          '@vexip-ui/icons': Object.keys(await import('@vexip-ui/icons')).map(name =>
-            name.match(/^I[0-9]/) ? name : [name, `I${name}`]
-          )
-        }
-      ]
-    }),
-    components({
-      resolvers: [VexipUIResolver()]
-    })
-  ]
+export default defineConfig(async ({ command }) => {
+  return {
+    optimizeDeps: {
+      include: ['vexip-ui', '@vexip-ui/icons']
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      autoImport({
+        vueTemplate: true,
+        resolvers: [
+          VexipUIResolver({
+            fullStyle: command === 'serve'
+          })
+        ],
+        imports: [
+          {
+            '@vexip-ui/icons': Object.keys(await import('@vexip-ui/icons')).map(name =>
+              name.match(/^I[0-9]/) ? name : [name, `I${name}`]
+            )
+          }
+        ]
+      }),
+      components({
+        resolvers: [
+          VexipUIResolver({
+            fullStyle: command === 'serve'
+          })
+        ]
+      })
+    ]
+  }
 })
